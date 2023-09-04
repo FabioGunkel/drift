2ecbbeacd90daeaa44a26703159a39b91ce27fc4
Fabio Gunkel <fabiogunkel@gmail.com>
2023-09-03 22:21:12 -0300

    configuring the libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gunkel.android.drift"
-    compileSdk = 32
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gunkel.android.drift"
@@ -34,10 +34,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.compat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junit.ext)
+    androidTestImplementation(libs.espresso)
 }
