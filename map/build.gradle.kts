8e649030c3d45dcd0fc3aaed26903f6b6ff80aaf
Fabio Gunkel <fabiogunkel@gmail.com>
2023-09-10 14:59:11 -0300

    adding compose

--- map/build.gradle.kts
+++ map/build.gradle.kts
@@ -15,6 +15,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -34,11 +37,16 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose =  true
+        compose = true
     }
 
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.2"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -47,10 +55,33 @@ dependencies {
     implementation(libs.androidx.ktx)
     implementation(libs.androidx.compat)
     implementation(libs.material)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui.graphics)
+    androidTestImplementation(platform(libs.compose.bom))
 
+    //Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.foundation:foundation")
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Optional - Integration with activities
+    implementation("androidx.activity:activity-compose:1.6.1")
+    // Optional - Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.compose.material3:material3")
 
     //Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.junit.ext)
     androidTestImplementation(libs.espresso)
+
+
 }
--- map/src/main/kotlin/com/gunkel/map/MapActivity.kt
+++ map/src/main/kotlin/com/gunkel/map/MapActivity.kt
@@ -2,12 +2,30 @@ package com.gunkel.map
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MapActivity : Activity() {
+
+class MapActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
-
+        setContent {
+            MapScreen()
         }
+    }
+
+    @Composable
+    private fun MapScreen() {
+        Text("Hello world!")
+    }
+
+
+    @Preview
+    @Composable
+    fun PreviewMapScreen() {
+        MapScreen()
     }
 }
